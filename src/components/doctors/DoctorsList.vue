dac1076a355054bc6e2a3867b85d43635907b022
AndreaGarofalo <andreasjobs94@outlook.it>
2023-04-03 02:14:44 +0200

    fixed routes and homepage

--- src/components/doctors/DoctorsList.vue
+++ src/components/doctors/DoctorsList.vue
@@ -11,19 +11,17 @@ export default {
 <template>
   <div class="completed-list">
     <h3 class="text-danger mt-4 mb-5 text-center">Dottori in evidenza</h3>
-    <h1 class="text-center pt-5">CARD ANDREA</h1>
     <div class="doctors-list d-flex justify-content-start flex-wrap mb-5">
-      <DoctorsCard v-for="doctor in doctors" :key="doctor.id" :doctor="doctor" />
+      <DoctorsCard
+        v-for="doctor in doctors"
+        :key="doctor.id"
+        :doctor="doctor"
+      />
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.completed-list {
-  border: 6px solid black;
-  height: 600px;
-}
-
 .doctors-list {
   gap: 15px;
   margin: 0 auto;
