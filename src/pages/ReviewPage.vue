37d7b1290f2c3ef9a58247ac2bac744bccd8ccf5
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-04-14 09:41:04 +0200

    remove novalidate

--- src/pages/ReviewPage.vue
+++ src/pages/ReviewPage.vue
@@ -47,38 +47,19 @@ export default {
   <div class="container p-2">
     <h1>Leave us a review</h1>
     <!-- Prevent e intercetta il method sendform -->
-    <form @submit.prevent="sendForm" class="py-5" novalidate>
+    <form @submit.prevent="sendForm" class="py-5">
       <!-- EMAIL -->
       <div class="mb-3">
-        <label for="name" class="form-label"
-          >Full name<sup class="text-danger">*</sup></label
-        >
-        <input
-          @keyup="this.getId()"
-          type="text"
-          class="form-control"
-          id="name"
-          name="name"
-          aria-describedby="name@example.com"
-          v-model.trim="form.name"
-          required
-        />
+        <label for="name" class="form-label">Full name<sup class="text-danger">*</sup></label>
+        <input @keyup="this.getId()" type="text" class="form-control" id="name" name="name"
+          aria-describedby="name@example.com" v-model.trim="form.name" required />
         <small class="text-muted form-text">Your full name</small>
       </div>
 
       <!-- CONTENUTO DEL MESSAGGIO -->
       <div class="mb-3">
-        <label for="text" class="form-label"
-          >Review<sup class="text-danger">*</sup></label
-        >
-        <textarea
-          class="form-control"
-          rows="3"
-          name="text"
-          id="text"
-          v-model.trim="form.text"
-          required
-        ></textarea>
+        <label for="text" class="form-label">Review<sup class="text-danger">*</sup></label>
+        <textarea class="form-control" rows="3" name="text" id="text" v-model.trim="form.text" required></textarea>
       </div>
 
       <div>
